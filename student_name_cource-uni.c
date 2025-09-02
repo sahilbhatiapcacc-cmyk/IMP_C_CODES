4883c9e7b3f6a8788e4f6a7e0930c1b58a4ef2ac
SAHIL BHA7IA <sahilbhatiapcacc@gmail.com>
2025-09-02 11:20:13 +0530

    Create student_name_cource-uni.c

---
+++ student_name_cource-uni.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    char name[90],c[90],uni[90];
+    printf("enter your name: ");
+    scanf("%s", name);
+    printf("enter your cource: ");
+    scanf("%s", c);
+    printf("enter your university name: ");
+    scanf("%s", uni);
+    printf("\n\nyour name : %s\n", name);
+    printf("your cource : %s\n", c);
+    printf("your university: %s\n", uni);
+    return 0;
+    
+}
