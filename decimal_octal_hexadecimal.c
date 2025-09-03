b8e90bc12f1a2b771cd022506e65a93fc276ea2e
SAHIL BHA7IA <sahilbhatiapcacc@gmail.com>
2025-09-03 10:24:39 +0530

    Create decimal_octal_hexadecimal.c

---
+++ decimal_octal_hexadecimal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int decimalNum;
+    printf("enter decimal number: ");
+    scanf("%d",&decimalNum);
+    
+    printf("decimal: %d\n",decimalNum);
+    printf("octal: %o\n",decimalNum);
+    printf("hexadecimal: %x\n",decimalNum);
+    printf("hexadecimal: %X\n",decimalNum);
+    return 0;
+
+
+}
