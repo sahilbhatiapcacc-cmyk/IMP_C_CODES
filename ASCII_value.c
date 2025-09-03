275753ae2d1b2ce43308dfe94d644ee4fe827bc3
SAHIL BHA7IA <sahilbhatiapcacc@gmail.com>
2025-09-03 09:54:33 +0530

    Create ASCII_value.c

---
+++ ASCII_value.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char ch;
+    printf("enter any character: ");
+    scanf("%c", &ch);
+    printf("THE ASCII VALUE OF %c is %d", ch ,ch);
+    return 0;
+    
+}
