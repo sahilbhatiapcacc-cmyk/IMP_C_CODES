9431f068926c413982aa0127b1d0cad973497435
SAHIL BHA7IA <sahilbhatiapcacc@gmail.com>
2025-09-04 13:42:00 +0530

    Create global_variable_counter.c

---
+++ global_variable_counter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int counter = 0;
+
+void increamentcounter(){
+  counter++;
+}
+
+
+int main(){
+  int n;
+  scanf("%d",&n);
+  
+  for (int i=0 ;i < n; i++){
+    increamentcounter();
+  }
+  
+  printf("Final counter: %d",counter);
+  return 0;
+  
+}
